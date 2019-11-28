69ec819ade43d816de517000222f19108cc94067
hjkim4518 <hjkim4518@cslee.co.kr>
2019-11-28 17:37:36 +0900

    Vue router 추가

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/dd',
+        name: 'test',
+        component: HelloWorld
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history', // 아마 url로 직접 접속하면 오류날 것
+    routes: routes
+})
+
+export default router
