69464d7c4f374c0f47cf267a78b70657d3bc628e
hjkim4518 <hjkim4518@cslee.co.kr>
2019-11-29 09:52:15 +0900

    history모드 대응 테스트2

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,20 @@ set -e
 echo 빌드합니다..
 npm run build
 
-# push
+cd docs
+cp index.html 404.html
+find . -name ".DS_Store" -delete
+
+# deploy
+echo 배포합니다..
 git add -A
 echo -e "커밋 메시지를 입력하세요: "
 read word
-git commit -m "$word"
+git commit -m "${word}"
 
 # deploy
 git push -u origin master
+
+# 루트 디렉토리로 나와 dist 디렉토리 제거
+cd -
+rm -rf docs
