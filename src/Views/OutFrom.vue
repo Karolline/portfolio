e13f7c41745721ff0db6019e9ecf91a39634c544
hjkim4518 <hjkim4518@cslee.co.kr>
2019-12-03 11:28:23 +0900

    배경이미지 svg > jpg 변경 및 출처 정보 모음

--- src/Views/OutFrom.vue
+++ src/Views/OutFrom.vue
@@ -10,3 +10,6 @@ AI
 <div>Icons made by <a href="https://www.flaticon.com/authors/prosymbols" title="Prosymbols">Prosymbols</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
 백그라운드 이미지
+https://www.freepik.com/search?dates=any&format=search&page=1&query=pastel&selection=1&sort=popular
+https://www.freepik.com/free-vector/abstract-watercolor-texture_3369818.htm#page=1&query=pastel&position=24#position=24&page=1&query=pastel
+<a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by s.salvador - www.freepik.com</a>
