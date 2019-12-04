08edead0a02f8efe5c045418895140f67ca7e2a7
hjkim4518 <hjkim4518@cslee.co.kr>
2019-12-04 17:31:02 +0900

    IntroCollapse 수정

--- src/components/IntroCollapse.vue
+++ src/components/IntroCollapse.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="intro-collapse">
         <b-row class="text-center">
             <b-col>
                 <img src="@/assets/images/browser.svg" v-b-toggle="'browser-content'">
@@ -25,19 +25,19 @@
         </b-row>
          <b-row class="text-center mt-5 mb-5">
             <b-col>
-                <b-collapse id="browser-content" accordion="intro-accordion" visible>
+                <b-collapse class="collpase-content" id="browser-content" accordion="intro-accordion" visible>
                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
                     Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
                     ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                     <!-- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. -->
                 </b-collapse>
-                <b-collapse id="big-data-content" accordion="intro-accordion">
+                <b-collapse class="collpase-content" id="big-data-content" accordion="intro-accordion">
                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
                     Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
                     ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                     <!-- Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. -->
                 </b-collapse>
-                <b-collapse id="ai-content" accordion="intro-accordion">
+                <b-collapse class="collpase-content" id="ai-content" accordion="intro-accordion">
                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
                     Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
                     ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
@@ -55,15 +55,28 @@ export default {
 </script>
 
 <style scoped>
-.collapse {
+#intro-collapse {
+    width: 80%;
+    margin: 0 auto;
+    /* padding: 60px 30px 60px 30px; */
+}
+
+.content-div img { /* 어디에 먹인거지 */
+    width: 100px;
+    background-color: rgba(255, 255, 255, 0.6);
+    border-radius: 2em;
+    padding: 20px 20px 20px 20px;
+}
+
+.collpase-content {
+    padding-left: 20px;
+}
+
+
+/* .collapsing {
     width: 50%;
     margin: 0 auto;
     padding: 60px 30px 60px 30px;
-}
-.collapsing {
-    width: 50%;
-    margin: 0 auto;
-    padding: 60px 30px 60px 30px;
-}
+} */
 /* collpase 말고 tab을 고려해보자 */
 </style>
