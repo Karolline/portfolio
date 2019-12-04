bcef9bf8a23dab2fe8ace5f0d50dfdaf13158f0b
hjkim4518 <hjkim4518@cslee.co.kr>
2019-12-04 17:20:00 +0900

    추가개발 아이디어 정 등

--- src/Views/Intro.vue
+++ src/Views/Intro.vue
@@ -1,10 +1,14 @@
 <template>
     <b-container class="content-div mt-5">
-        <b-row class="text-center mt-5">
+        <!-- 작은 내비게이션바 만들어서 특정 div로 이동시키기 할까? -->
+        <b-row class="text-center mt-5 mb-2">
             <b-col><h3>인생 그래프</h3></b-col>
         </b-row>
         <b-row>
-            <life-graph />
+            <life-graph style="margin: 0 auto" />
+        </b-row>
+        <b-row class="text-center">
+            <b-col><a href="#">내 인생 그래프 만들어보기</a></b-col>
         </b-row>
 
         <b-row class="text-center mt-5 mb-5">
@@ -33,3 +37,7 @@ export default {
     
 }
 </script>
+
+<style scoped>
+
+</style>
--- src/components/IntroLifeGraph.vue
+++ src/components/IntroLifeGraph.vue
@@ -1,5 +1,7 @@
 <template>
     <Plotly :data="data" :layout="layout" :displayModeBar="false"></Plotly>
+    <!-- 반응형으로 만들기 -->
+    <!-- 확대불가능하게 만들기 -->
 </template>
 
 <script>
